4b9a184bf98be3dcefe6dcf3025a7c5741d2b84d
Viktor Blazhevski <victordeflos@hotmail.com>
2021-03-26 17:38:41 +0100

    Initial commit

---
+++ src/actions/types.js
@@ -0,0 +1,7 @@
+/** @format */
+
+export const GET_POST = 'GET_POST';
+export const CREATE_POST = 'CREATE_POST';
+export const UPDATE_POST = 'UPDATE_POST';
+export const DELETE_POST = 'DELETE_POST';
+export const GET_POSTS = 'GET_POSTS';
